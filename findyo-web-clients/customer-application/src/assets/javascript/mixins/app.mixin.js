12120f5fbcf01950354b897e8b36e3d30edb72d9
Thenukad Dinu <thenukadinu@gmail.com>
2021-08-23 23:49:37 +0530

    add missing files.

---
+++ findyo-web-clients/customer-application/src/assets/javascript/mixins/app.mixin.js
@@ -0,0 +1,43 @@
+export default {
+  data () {
+    return {
+      profileData: {
+        email: undefined,
+        last_name: undefined,
+        displayName: undefined,
+        first_name: undefined,
+        username: undefined,
+        district: undefined,
+        creationTime: undefined,
+        phoneNumber: undefined,
+        postal_code: undefined,
+        address: undefined,
+        emailVerified: undefined,
+        job_title: undefined,
+        country: undefined,
+        province: undefined,
+        city: undefined,
+        id: undefined
+      },
+      userData: {
+        creationTime: undefined,
+        job_title: undefined,
+        address: undefined,
+        postal_code: undefined,
+        displayName: undefined,
+        username: undefined,
+        phoneNumber: undefined,
+        district: undefined,
+        email: undefined,
+        country: undefined,
+        last_name: undefined,
+        province: undefined,
+        emailVerified: false,
+        first_name: undefined,
+        city: undefined,
+        id: undefined
+      }
+    }
+  },
+  methods: {}
+}
